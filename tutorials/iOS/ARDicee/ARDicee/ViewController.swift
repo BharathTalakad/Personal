8f938d4423d64927d15f2b448241945e2556be4f
bharath <bharath@mercari.com>
2019-01-24 09:42:48 +0900

    Added functions to rollall dice on clicking a button or through shaking the phone

--- tutorials/iOS/ARDicee/ARDicee/ViewController.swift
+++ tutorials/iOS/ARDicee/ARDicee/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var diceArray = [SCNNode]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -51,6 +53,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     
+    @IBAction func rollAgain(_ sender: UIBarButtonItem) {
+        rollAll()
+    }
+    
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        rollAll()
+    }
+    
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first{
             let touchLocation = touch.location(in: sceneView)
@@ -68,24 +80,37 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                     hitResult.worldTransform.columns.3.x,
                     hitResult.worldTransform.columns.3.y  + diceNode.boundingSphere.radius ,
                     hitResult.worldTransform.columns.3.z)
+                    
+                    diceArray.append(diceNode)
         
                 sceneView.scene.rootNode.addChildNode(diceNode)
                     
                     
-                    
-                let randomX = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
-                let randomz = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
-                    
-                diceNode.runAction(
-                    SCNAction.rotateBy(
-                        x: CGFloat(randomX * 5),
-                        y: CGFloat(0),
-                        z: CGFloat(randomz * 5),
-                        duration: 0.5)
-                    )
+                    roll(dice: diceNode)
             }
         }
         }
+    }
+    
+    func rollAll(){
+        if !diceArray.isEmpty{
+            for dice in diceArray{
+                roll(dice: dice)
+            }
+        }
+    }
+    
+    func roll(dice: SCNNode){
+        let randomX = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
+        let randomz = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
+        
+        dice.runAction(
+            SCNAction.rotateBy(
+                x: CGFloat(randomX * 5),
+                y: CGFloat(0),
+                z: CGFloat(randomz * 5),
+                duration: 0.5)
+        )
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -133,16 +158,5 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             return
         }
     }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
  
 }
